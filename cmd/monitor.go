7e0a1feb350e46e26ea2f15057d90692d0b803e1
zloper <makiavel@mail.ru>
2019-06-19 01:59:56 +0300

    first commit

---
+++ cmd/monitor.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"monitor/controler"
+	"net/http"
+	"strings"
+)
+
+type Conf struct {
+	Port string `json:"port"`
+}
+
+func main() {
+	// TODO Call every 10 sec stat for all connected services,
+	//  cli for add services to monitor
+	//  cli for stop/start
+	//  cli for service creation
+
+	jFile, err := ioutil.ReadFile(controler.CFG_PATH)
+	if err != nil {
+		panic(err)
+	}
+	var cfg Conf
+	err = json.Unmarshal(jFile, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	monitor := controler.NewServiceController()
+
+	router := gin.Default()
+	router.GET("/monitor/:cmd/:name", func(gctx *gin.Context) {
+		name := strings.ToLower(strings.TrimSpace(gctx.Param("name")))
+		cmd := strings.ToLower(strings.TrimSpace(gctx.Param("cmd")))
+
+		if cmd == "" {
+			gctx.String(http.StatusOK, "")
+			return
+		}
+		response := cmd
+
+		if name == "" {
+			if cmd == controler.STAT {
+				response = strings.Join(monitor.RefreshStatus(), " ")
+			}
+			gctx.String(http.StatusOK, response)
+			return
+		}
+
+		if cmd == controler.STOP {
+			err = monitor.Stop(name)
+			if err != nil {
+				panic(err)
+			}
+		} else if cmd == controler.RUN {
+			err = monitor.Run(name)
+			if err != nil {
+				panic(err)
+			}
+		} else if cmd == controler.RESTART {
+			err = monitor.Restart(name)
+			if err != nil {
+				panic(err)
+			}
+		} else if cmd == controler.STAT {
+			response = monitor.Status(name)
+			if err != nil {
+				panic(err)
+			}
+		}
+		gctx.String(http.StatusOK, response)
+		return
+
+	})
+	panic(router.Run(cfg.Port))
+}
---
+++ controler/consts.go
@@ -0,0 +1,10 @@
+package controler
+
+const (
+	COMMAND  = "service"
+	STAT     = "status"
+	STOP     = "stop"
+	RUN      = "run"
+	RESTART  = "restart"
+	CFG_PATH = "config.json"
+)
